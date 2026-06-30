898afb85f9a7a6047f28b3824501c6202d949fa9
jctrow70-cyber <jctrow70@gmail.com>
2026-06-30 16:10:29 -0400

    w

--- app/NutritionPage.tsx
+++
@@ -1,6 +0,0 @@
-import NutritionTracker from "../components/NutritionTracker";
-import "../components/NutritionTracker.css";
-
-export default function NutritionPage() {
-  return <NutritionTracker />;
-}
