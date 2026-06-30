75174d1a360ce45a14192718453069803bee9ff6
jctrow70-cyber <jctrow70@gmail.com>
2026-06-30 15:56:45 -0400

    nutrition phase 1

---
+++ src/pages/NutritionPage.tsx
@@ -0,0 +1,6 @@
+import NutritionTracker from "../components/NutritionTracker";
+import "../components/NutritionTracker.css";
+
+export default function NutritionPage() {
+  return <NutritionTracker />;
+}
