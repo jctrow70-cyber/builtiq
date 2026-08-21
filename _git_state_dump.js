a08fae41107f7bfaea7ef84a1042adbdd79d6217
jctrow70-cyber <jctrow70@gmail.com>
2026-08-20 20:15:57 -0400

    Update _git_state_dump.js

--- _git_state_dump.js
+++ _git_state_dump.js
@@ -1,6 +1,8 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
-const cwd = __dirname;
+const path = require('path');
+const cwd = 'C:/Users/JesseTrowbridge/OneDrive - Tegria/Documents/GitHub/builtiq';
+const out = path.join(cwd, '_git_state_dump.txt');
 const g = (args) => execSync(`git -c core.pager=cat ${args}`, { cwd, encoding: 'utf8' });
 const lines = [];
 const add = (title, cmd) => {
@@ -8,19 +10,15 @@ const add = (title, cmd) => {
   try {
     lines.push(g(cmd));
   } catch (e) {
-    lines.push(String(e.stdout || e.message));
+    lines.push(String(e.stdout || e.stderr || e.message));
   }
 };
-add('status', 'status');
+add('status', 'status --short --branch');
 add('head', 'rev-parse --abbrev-ref HEAD');
 add('branch -vv', 'branch -vv');
 add('branch -a nutrition', 'branch -a --list "*nutrition*"');
-add('log -15', 'log --oneline -15');
-add('main log -5', 'log --oneline -5 main');
-add('preview log -5', 'log --oneline -5 preview/nutrition-ux-biq-0135');
-add('main..preview', 'log --oneline main..preview/nutrition-ux-biq-0135');
-add('preview..main', 'log --oneline preview/nutrition-ux-biq-0135..main');
+add('log -8', 'log --oneline -8');
+add('main..HEAD', 'log --oneline main..HEAD');
+add('HEAD..main', 'log --oneline HEAD..main');
 add('remote -v', 'remote -v');
-add('branch -r', 'branch -r');
-fs.writeFileSync('_git_state_dump.txt', lines.join(''), 'utf8');
-console.log('written _git_state_dump.txt');
+fs.writeFileSync(out, lines.join(''), 'utf8');
